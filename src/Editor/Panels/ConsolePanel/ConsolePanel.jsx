e1019854a7aa3d6c884ab2053d9a841c120602bc
pkhead <chromosoze@gmail.com>
2022-03-05 22:51:40 -0600

    fix console scroll

--- src/Editor/Panels/ConsolePanel/ConsolePanel.jsx
+++ src/Editor/Panels/ConsolePanel/ConsolePanel.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ActionButton from 'Editor/Util/ActionButton/ActionButton';
 import { Console } from 'console-feed'
+import { ReflexContainer, ReflexElement } from 'react-reflex'
 
 export default function ConsolePanel(props) {
     function clearConsole() {
@@ -9,22 +10,27 @@ export default function ConsolePanel(props) {
 
     return (
         <div className="wick-code-editor-console">
-            <div className="we-code-console-bar">
-                <div className="outliner-title-name">Console</div>
-                <div className="we-code-console-options-container">
-                    <ActionButton
-                    className="we-code-console-option we-code-clear-console"
-                    id="clear-console-button"
-                    icon="clear"
-                    action={clearConsole}
-                    tooltip="Clear Console"
-                    tooltipPlace="left"
-                    color='tool' />
-                </div>
-
-            </div>
-            
-            <Console logs={props.consoleLogs} variant="dark"/>
+            <ReflexContainer orientation="horizontal">
+                <ReflexElement
+                size={40} // ok????????? not in pixels?? what unit is this?
+                className="we-code-console-bar"> {/* we-code-console-bar */}
+                    <div className="outliner-title-name">Console</div>
+                    <div className="we-code-console-options-container">
+                        <ActionButton
+                        className="we-code-console-option we-code-clear-console"
+                        id="clear-console-button"
+                        icon="clear"
+                        action={clearConsole}
+                        tooltip="Clear Console"
+                        tooltipPlace="left"
+                        color='tool' />
+                    </div>
+                </ReflexElement>
+                
+                <ReflexElement>
+                    <Console logs={props.consoleLogs} variant="dark"/>
+                </ReflexElement>
+            </ReflexContainer>
         </div>
     )
 }
